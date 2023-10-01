a8d1685e34185febebd6d3ebd20075449cd59b40
Kyon Lee <kyon.h.lee@gmail.com>
2023-10-01 17:36:24 +1100

    Add kyonlee_bongocat_wpm keymaps

---
+++ keyboards/lily58/keymaps/kyonlee_bongocat_wpm/config.h
@@ -0,0 +1,35 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Select hand configuration
+#define SPLIT_USB_DETECT
+#define MASTER_LEFT
+
+// Split Adjust
+#define SPLIT_USB_TIMEOUT 2000
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_WATCHDOG_TIMEOUT 3000
+
+// Oled config
+#define SPLIT_WPM_ENABLE
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
