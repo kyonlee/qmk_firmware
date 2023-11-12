1ac9e1d9d8764e3a74330b70ef250412a50f43ec
Kyon Lee <kyon.h.lee@gmail.com>
2023-11-12 19:41:52 +1100

    Revert "Add LOCK macro"
    This reverts commit f13404b1b3fad814abc9940552135b17c9500290.

--- keyboards/lily58/keymaps/kyonlee_bongocat_wpm/keymap.c
+++ keyboards/lily58/keymaps/kyonlee_bongocat_wpm/keymap.c
@@ -18,10 +18,6 @@ uint8_t current_tap_frame = 0;
 
 static long int oled_timeout = 60000; // 1 minute
 
-enum custom_keycodes {
-    LOCK = SAFE_RANGE
-};
-
 enum layer_number {
   _QWERTY = 0,
   _LOWER,
@@ -33,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | LOCK |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -47,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    LOCK,    \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
@@ -121,20 +117,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        // Mac OS X lock screen macro
-        case LOCK:
-            if (record->event.pressed) {
-                SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LGUI)SS_DOWN(X_Q));
-            } else {
-                SEND_STRING(SS_UP(X_LCTRL)SS_UP(X_LGUI)SS_UP(X_Q));
-            }
-            break;
-    }
-    return true;
 }
 
 //
